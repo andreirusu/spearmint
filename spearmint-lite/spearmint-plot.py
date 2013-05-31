0a27ff0817af54a0532ab98e7cf8bb24bca813b1
Julien Cornebise <julien.cornebise@gmail.com>
2013-05-31 18:37:37 +0100

    Remove multiple jobs option

--- spearmint-lite/spearmint-plot.py
+++ spearmint-lite/spearmint-plot.py
@@ -53,9 +53,6 @@ except ImportError: import json
 def main():
     parser = optparse.OptionParser(usage="usage: %prog [options] directory")
 
-    parser.add_option("--n", dest="num_jobs",
-                      help="Number of concurrent jobs to create.",
-                      type="int", default=1)
     parser.add_option("--max-finished-jobs", dest="max_finished_jobs",
                       type="int", default=1000)
     parser.add_option("--method", dest="chooser_module",
@@ -107,111 +104,108 @@ def main_controller(options, args):
     #vkeys.sort()
     gmap = GridMap([variables[k] for k in vkeys], options.grid_size)
 
-    # Read in parameters and values observed so far
-    for i in xrange(0,options.num_jobs):
+    res_file = os.path.join(expt_dir, options.results_file)
+    if not os.path.exists(res_file):
+        thefile = open(res_file, 'w')
+        thefile.write("")
+        thefile.close()
 
-        res_file = os.path.join(expt_dir, options.results_file)
-        if not os.path.exists(res_file):
-            thefile = open(res_file, 'w')
-            thefile.write("")
-            thefile.close()
+    values = np.array([])
+    complete = np.array([])
+    pending = np.array([])
+    durations = np.array([])
+    index = 0
 
-        values = np.array([])
-        complete = np.array([])
-        pending = np.array([])
-        durations = np.array([])
-        index = 0
+    infile = open(res_file, 'r')
+    for line in infile.readlines():
+        # Each line in this file represents an experiment
+        # It is whitespace separated and of the form either
+        # <Value> <time taken> <space separated list of parameters>
+        # incating a completed experiment or
+        # P P <space separated list of parameters>
+        # indicating a pending experiment
+        expt = line.split()
+        if (len(expt) < 3):
+            continue
 
-        infile = open(res_file, 'r')
-        for line in infile.readlines():
-            # Each line in this file represents an experiment
-            # It is whitespace separated and of the form either
-            # <Value> <time taken> <space separated list of parameters>
-            # incating a completed experiment or
-            # P P <space separated list of parameters>
-            # indicating a pending experiment
-            expt = line.split()
-            if (len(expt) < 3):
-                continue
-
-            val = expt.pop(0)
-            dur = expt.pop(0)
-            variables = gmap.to_unit(expt)
-            if val == 'P':
-                if pending.shape[0] > 0:
-                    pending = np.vstack((pending, variables))
-                else:
-                    pending = np.matrix(variables)
+        val = expt.pop(0)
+        dur = expt.pop(0)
+        variables = gmap.to_unit(expt)
+        if val == 'P':
+            if pending.shape[0] > 0:
+                pending = np.vstack((pending, variables))
             else:
-                if complete.shape[0] > 0:
-                    values = np.vstack((values, float(val)))
-                    complete = np.vstack((complete, variables))
-                    durations = np.vstack((durations, float(dur)))
-                else:
-                    values = float(val)
-                    complete = np.matrix(variables)
-                    durations = float(dur)
-                
-        infile.close()
-        # Some stats
-        sys.stderr.write("#Complete: %d #Pending: %d\n" % 
-                         (complete.shape[0], pending.shape[0]))
-
-        # Let's print out the best value so far
-        if type(values) is not float and len(values) > 0:
-            best_val = np.min(values)
-            best_job = np.argmin(values)
-            sys.stderr.write("Current best: %f (job %d)\n" % (best_val, best_job))
-    
-        # Now lets get the next job to run
-        # First throw out a set of candidates on the unit hypercube
-        # Increment by the number of observed so we don't take the
-        # same values twice
-        off = pending.shape[0] + complete.shape[0]
-        candidates = gmap.hypercube_grid(options.grid_size,
-                                         options.grid_seed+off)
-
-        # Ask the chooser to actually pick one.
-        # First mash the data into a format that matches that of the other
-        # spearmint drivers to pass to the chooser modules.        
-        grid = candidates
-        if (complete.shape[0] > 0):
-            grid = np.vstack((complete, candidates))
-        if (pending.shape[0] > 0):
-            grid = np.vstack((grid, pending))
-        grid = np.asarray(grid)
-        grid_idx = np.hstack((np.zeros(complete.shape[0]),
-                              np.ones(candidates.shape[0]),
-                              1.+np.ones(pending.shape[0])))
-        job_id = chooser.next(grid, np.squeeze(values), durations,
-                              np.nonzero(grid_idx == 1)[0],
-                              np.nonzero(grid_idx == 2)[0],
-                              np.nonzero(grid_idx == 0)[0])
-        
-        # If the job_id is a tuple, then the chooser picked a new job not from
-        # the candidate list
-        if isinstance(job_id, tuple):
-            (job_id, candidate) = job_id
+                pending = np.matrix(variables)
         else:
-            candidate = candidates[job_id,:]
-
-        sys.stderr.write("Selected job %d from the grid.\n" % (job_id))
-        if pending.shape[0] > 0:
-            pending = np.vstack((pending, candidate))
-        else:
-            pending = np.matrix(candidate)
-
-        params = gmap.unit_to_list(candidate)
-
-        # Now lets write this candidate to the file as pending
-        output = ""
-        for p in params:
-            output = output + str(p) + " "
+            if complete.shape[0] > 0:
+                values = np.vstack((values, float(val)))
+                complete = np.vstack((complete, variables))
+                durations = np.vstack((durations, float(dur)))
+            else:
+                values = float(val)
+                complete = np.matrix(variables)
+                durations = float(dur)
             
-        output = "P P " + output + "\n"
-        outfile = open(res_file,"a")
-        outfile.write(output)
-        outfile.close()        
+    infile.close()
+    # Some stats
+    sys.stderr.write("#Complete: %d #Pending: %d\n" % 
+                     (complete.shape[0], pending.shape[0]))
+
+    # Let's print out the best value so far
+    if type(values) is not float and len(values) > 0:
+        best_val = np.min(values)
+        best_job = np.argmin(values)
+        sys.stderr.write("Current best: %f (job %d)\n" % (best_val, best_job))
+
+    # Now lets get the next job to run
+    # First throw out a set of candidates on the unit hypercube
+    # Increment by the number of observed so we don't take the
+    # same values twice
+    off = pending.shape[0] + complete.shape[0]
+    candidates = gmap.hypercube_grid(options.grid_size,
+                                     options.grid_seed+off)
+
+    # Ask the chooser to actually pick one.
+    # First mash the data into a format that matches that of the other
+    # spearmint drivers to pass to the chooser modules.        
+    grid = candidates
+    if (complete.shape[0] > 0):
+        grid = np.vstack((complete, candidates))
+    if (pending.shape[0] > 0):
+        grid = np.vstack((grid, pending))
+    grid = np.asarray(grid)
+    grid_idx = np.hstack((np.zeros(complete.shape[0]),
+                          np.ones(candidates.shape[0]),
+                          1.+np.ones(pending.shape[0])))
+    job_id = chooser.next(grid, np.squeeze(values), durations,
+                          np.nonzero(grid_idx == 1)[0],
+                          np.nonzero(grid_idx == 2)[0],
+                          np.nonzero(grid_idx == 0)[0])
+    
+    # If the job_id is a tuple, then the chooser picked a new job not from
+    # the candidate list
+    if isinstance(job_id, tuple):
+        (job_id, candidate) = job_id
+    else:
+        candidate = candidates[job_id,:]
+
+    sys.stderr.write("Selected job %d from the grid.\n" % (job_id))
+    if pending.shape[0] > 0:
+        pending = np.vstack((pending, candidate))
+    else:
+        pending = np.matrix(candidate)
+
+    params = gmap.unit_to_list(candidate)
+
+    # Now lets write this candidate to the file as pending
+    output = ""
+    for p in params:
+        output = output + str(p) + " "
+        
+    output = "P P " + output + "\n"
+    outfile = open(res_file,"a")
+    outfile.write(output)
+    outfile.close()        
 
 # And that's it
 if __name__ == '__main__':
