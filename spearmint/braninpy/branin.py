cdf1740692382172802b33f8d12574d1e05672a5
Julien Cornebise <julien.cornebise@gmail.com>
2013-06-04 10:54:49 +0100

    Fix branin function in spearmint too

--- spearmint/braninpy/branin.py
+++ spearmint/braninpy/branin.py
@@ -11,8 +11,8 @@ def branin(x):
   if x[1] <0 or x[1] > 1:
     return np.NaN
 
-  x[0] = x[0]*15
-  x[1] = (x[1]*15)-5
+  x[0] = (x[0]*15)-5
+  x[1] = x[1]*15
 
   y = np.square(x[1] - (5.1/(4*np.square(math.pi)))*np.square(x[0]) + (5/math.pi)*x[0] - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x[0]) + 10;
 
