716cd6dd5a6eb390e806c6b9b9c556bb53ddb6e6
Andrei Rusu <andreirusu@deepmind.com>
2013-06-10 16:22:01 +0100

    Minor refactoring of job script.

--- spearmint/xae-sge-mnist/job.sh
+++ spearmint/xae-sge-mnist/job.sh
@@ -22,15 +22,19 @@ COMMON_OPTIONS="  -dataset $DATASET -threads $SPEARMINT_JOB_THEREADS -maxUpdates
 
 # build the deep network
 
-ae new -dir $JOBDIR/expdir_layer0  -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER0 $COMMON_OPTIONS -seed "$RANDOM"
+cmd1="ae new    -dir $JOBDIR/expdir_layer0 -seed $RANDOM -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER0 $COMMON_OPTIONS"
+cmd2="ae stack $JOBDIR/expdir_layer0    -dir $JOBDIR/expdir_layer1 -seed $RANDOM -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER1 $COMMON_OPTIONS" 
+cmd3="ae stack $JOBDIR/expdir_layer1    -dir $JOBDIR/expdir_layer2 -seed $RANDOM -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER2 $COMMON_OPTIONS"
+cmd4="ae-export-features    -input $JOBDIR/expdir_layer2    -format tensor  -output $JOBDIR/reps/last   -dataset $DATASET_SUP"
+cmd5="ae-svm-test   -r $JOBDIR/reps   -f $JOBDIR/res.txt $SPEARMINT_JOB_TEST_OPTIONS"
 
-ae stack $JOBDIR/expdir_layer0 -dir $JOBDIR/expdir_layer1 -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER1 $COMMON_OPTIONS -seed "$RANDOM" 
+echo $cmd1
+echo $cmd2
+echo $cmd3
+echo $cmd4
+echo $cmd5
 
-ae stack $JOBDIR/expdir_layer1 -dir $JOBDIR/expdir_layer2 -encoder $ENCODER -decoder $DECODER $SPEARMINT_JOB_OPTIONS_LAYER2 $COMMON_OPTIONS -seed "$RANDOM"
-
-ae-export-features -input $JOBDIR/expdir_layer2 -format tensor -output $JOBDIR/reps/last -dataset $DATASET_SUP
-
-ae-svm-test -r $JOBDIR/reps -f $JOBDIR/res.txt $SPEARMINT_JOB_TEST_OPTIONS
+$cmd1 && $cmd2 && $cmd3 && $cmd4 && $cmd5 
 
 rm -Rf $JOBDIR/reps
 cd ..
