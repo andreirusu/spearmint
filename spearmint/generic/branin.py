88d129cedff33190db2d3c1a49ea709eda8f403f
Andrei A. Rusu <andreirusu@deepmind.com>
2013-07-26 14:11:58 +0100

    Removed sleep from branin.py

--- spearmint/generic/branin.py
+++ spearmint/generic/branin.py
@@ -17,7 +17,7 @@ def branin(x, y):
   result = np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) + (5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10;
 
   print 'Result: ', result
-  time.sleep(600)
+  #time.sleep(600)
   return result
 
 
