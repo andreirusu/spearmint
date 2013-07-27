a5151a6c86a820c37a7146d0b01d2343fbe5d51e
Andrei A. Rusu <andreirusu@deepmind.com>
2013-07-27 11:12:16 +0100

    Changed default strategy

--- spearmint/generic/example_run.sh
+++ spearmint/generic/example_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export GPMIN_MAX_CONCURRENT=500
-export GPMIN_MAX_JOBS=2000
+export GPMIN_MAX_CONCURRENT=100
+export GPMIN_MAX_JOBS=5000
 gpmin braninconfig.pb  python $PWD/branin.py  
 
