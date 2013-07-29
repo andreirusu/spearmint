abba99c4892c0e52dbb14a954041f2c9412b3cf1
Andrei A. Rusu <andreirusu@deepmind.com>
2013-07-29 13:59:53 +0100

    Working version of evaluation with annealed epsW

--- spearmint/cuda-convnet/job.sh
+++ spearmint/cuda-convnet/job.sh
@@ -26,8 +26,8 @@ echo $cost_out_filename
 
 
 ### WRITE CUDA-CONVNET CONFIG FILES
-sh $EXP_DIR/layer-params-conv-local-11pct.cfg.sh  
-sh $EXP_DIR/layers-conv-local-11pct.cfg.sh
+bash $EXP_DIR/layer-params-conv-local-11pct.cfg.sh  
+bash $EXP_DIR/layers-conv-local-11pct.cfg.sh
 
 
 function evaluate {
@@ -46,13 +46,13 @@ function evaluate {
     time python $NET_DIR/convnet.py --seed=$RANDOM_SEED --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=$SAVE_PATH --test-range=5 --train-range=1-4 --layer-def=$PWD/layers.cfg --layer-params=$PWD/params.cfg --data-provider=cifar-cropped --test-freq=$(( 4 * $epochs_stage1 ))  --crop-border=4 --epochs=$epochs_stage1 --gpu=$GPU_ID
 
     ### Then add 5th batch to training and test on the 6th; continue training for about 150 epochs
-    time python $NET_DIR/convnet.py -f $SAVE_PATH/ConvNet* --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=$SAVE_PATH --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --test-freq=$(( 5 * $epochs_stage2 )) --epochs=$(( $epochs_stage1 + $epochs_stage2 )) --gpu=$GPU_ID
+    time python $NET_DIR/convnet.py -f $SAVE_PATH/ConvNet*   --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=$SAVE_PATH  --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --test-freq=$(( 5 * $epochs_stage2 )) --epochs=$(( $epochs_stage1 + $epochs_stage2 )) --gpu=$GPU_ID
 
     ### Then lower learing rate 10 times (uncomment in layer params file); continue training for about 10 epochs
-    #time python convnet.py -f ../tmp/tmp/11p3d/ConvNet__2013-07-21_14.02.12 --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=/Users/andreirusu/funspace/tmp/11p --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --layer-params=/Users/andreirusu/funspace/cuda-convnet/example-layers/layer-params-conv-local-11pct-3d.cfg --test-freq=10 --epochs 510 --gpu 0
+    time python $NET_DIR/convnet.py -f $SAVE_PATH/ConvNet*   --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=$SAVE_PATH  --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --test-freq=5 --layer-params=$PWD/params_lower_epsW_10.cfg --epochs=$(( $epochs_stage1 + $epochs_stage2 + 50 )) --gpu=$GPU_ID
 
     ### Then lower learing rate 10 times once again (uncomment in layer params file); continue training for about 10 epochs
-    #time python convnet.py -f ../tmp/tmp/tmp/11p3d/ConvNet__2013-07-21_14.02.12 --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=/Users/andreirusu/funspace/tmp/11p --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --test-freq=5 --layer-params=/Users/andreirusu/funspace/cuda-convnet/example-layers/layer-params-conv-local-11pct-3d.cfg --epochs 520 --gpu 0
+    time python $NET_DIR/convnet.py -f $SAVE_PATH/ConvNet*   --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --save-path=$SAVE_PATH  --test-range=6 --train-range=1-5 --data-provider=cifar-cropped --test-freq=5 --layer-params=$PWD/params_lower_epsW_100.cfg  --epochs=$(( $epochs_stage1 + $epochs_stage2 + 100)) --gpu=$GPU_ID
 
     ### TEST => 0.1111 error; accuracy > 88% 
     time python $NET_DIR/convnet.py -f $SAVE_PATH/ConvNet* --logreg-name=logprob --data-path=/Users/andreirusu/funspace/cifar-10-py-colmajor --test-only=1 --multiview-test=1 --test-range=6 --data-provider=cifar-cropped --gpu $GPU_ID > $SAVE_PATH/test.txt
@@ -84,6 +84,6 @@ done
 avg=`echo "$avg / $count" | bc -l`
 
 
-#### WRITE RESUL
+#### WRITE RESULT
 echo $avg > $cost_out_filename
 
--- spearmint/cuda-convnet/layer-params-conv-local-11pct.cfg.sh
+++ spearmint/cuda-convnet/layer-params-conv-local-11pct.cfg.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
-layer_conf="[conv1]
-epsW=$epsW
+function dec {
+    printf "%.10f" $1
+}
+
+function div {
+    e=`printf "%.10f / %.10f" $1 $2`
+    res=`echo $e | bc -l`
+    dec $res
+}
+
+
+function layer_conf { 
+echo "[conv1]
+epsW=`div $epsW $1`
 epsB=0.002
 momW=0.9
 momB=0.9
-wc=$wc_conv1
+wc=`dec $wc_conv1`
 
 [conv2]
-epsW=$epsW
+epsW=`div $epsW $1`
 epsB=0.002
 momW=0.9
 momB=0.9
-wc=$wc_conv2
+wc=`dec $wc_conv2`
 
 [local3]
-epsW=$epsW
+epsW=`div $epsW $1`
 epsB=0.002
 momW=0.9
 momB=0.9
-wc=$wc_local3
+wc=`dec $wc_local3`
 
 [local4]
-epsW=$epsW
+epsW=`div $epsW $1`
 epsB=0.002
 momW=0.9
 momB=0.9
-wc=$wc_local4
+wc=`dec $wc_local4`
 
 [fc10]
-epsW=$epsW
+epsW=`div $epsW $1`
 epsB=0.002
 momW=0.9
 momB=0.9
-wc=$wc_fc10
+wc=`dec $wc_fc10`
 
 [logprob]
 coeff=1
 
 [rnorm1]
-scale=$scale_rnorm
-pow=$pow_rnorm
+scale=`dec $scale_rnorm`
+pow=`dec $pow_rnorm`
 
 [rnorm2]
-scale=$scale_rnorm
-pow=$pow_rnorm
- "
+scale=`dec $scale_rnorm`
+pow=`dec $pow_rnorm`
+     "
+}
 
-echo $layer_conf | tr " " "\n" > "params.cfg"
+layer_conf 1 | tr " " "\n" > "params.cfg"
+layer_conf 10 | tr " " "\n" > "params_lower_epsW_10.cfg"
+layer_conf 100 | tr " " "\n" > "params_lower_epsW_100.cfg"
 
